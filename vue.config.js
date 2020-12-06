b6b9eb30fa489ccb72201938f884584c7b6bf2ee
Mr.H <44597139+osorso@users.noreply.github.com>
2020-12-06 19:46:42 +0800

    fix: 修复发布包体积过大问题; update: 更新tinymce为5.6.1

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,47 @@
 const path = require('path')
 module.exports = {
+  productionSourceMap: false,
   pages: {
     index: {
       entry: 'examples/main.js',
       template: 'public/index.html',
+      // 发布npm时设置filename
       filename: 'tinymce-vue-h.min.js'
+    }
+  },
+  configureWebpack: config => {
+    config.optimization = {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            test: /node_modules/,
+            name: 'vendor',
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 100
+          },
+          common: {
+            chunks: 'all',
+            test: /[\\/]src[\\/]js[\\/]/,
+            name: 'common',
+            minChunks: 2,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 60
+          },
+          styles: {
+            name: 'styles',
+            test: /\.(sa|sc|c)ss$/,
+            chunks: 'all',
+            enforce: true
+          },
+          runtimeChunk: {
+            name: 'manifest'
+          }
+        }
+      }
     }
   },
   chainWebpack: config => {
@@ -21,5 +58,5 @@ module.exports = {
       .tap(options => {
         return options
       })
-  }
+  },
 }
